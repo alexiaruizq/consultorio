e15dbabbd9c257fa6fb4203719b9e233519bcbfc
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:42:09 -0700

    Agregar archivos de proyecto.

---
+++ Hola/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola
+{
+    class Paciente
+    {
+        //Atributos (variables)
+        string nombre;
+        string direccion;
+        string telefono;
+        DateTime fechaDeNacimiento;
+        string enfermedadescronicas;
+    }
+}
